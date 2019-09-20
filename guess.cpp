da1d26aa2c78dd12f46d1cbb76fe06f847143266
Harsh Singh <53997601+Analystharsh@users.noreply.github.com>
2019-09-20 20:51:54 +0530

    Create guess.cpp

---
+++ guess.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+  string a ="Harsh";
+  string b;
+  while(a!=b)
+  {
+     cout<<"Guess Name"<<endl;
+     cin>>b;
+  }
+  cout<<"You won";
+
+return 0;
+}
