37b61ba03c0c0ab505005b0c2379e37fad211564
Harsh Singh <53997601+Analystharsh@users.noreply.github.com>
2019-09-20 20:31:18 +0530

    Create bool.cpp

---
+++ bool.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+#define newline "\n"
+int main()
+{ 
+  
+bool isman =false;
+bool iswoman=true;
+
+if(isman&&iswoman)
+{
+cout<<"Your are a boss"<<endl;
+}
+ else
+ {cout<<"Your are a dog"<<endl;}
+
+return 0;
+}
